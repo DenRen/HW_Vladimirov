34b918fd2e98f98f3fdaa4c604becd78361f1bed
DenRen <denrenruslan@gmail.com>
2021-02-12 17:26:38 +0300

    Добавил нормальные переборы элементов матрицы и оператор [][]

--- MxLib/main.cpp
+++ MxLib/main.cpp
@@ -15,5 +15,17 @@ int main (int argc, char *argv[]) {
     genmx::Matrix <float> m = genmx::Matrix <float>::eye (3, 3);
     m.SetValue (0, 1, 5);
 
-    cout << m << endl << m.T ();
+    cout << m << endl << m.T () << endl;
+
+    m *= 5;
+
+    cout << m << endl;
+
+    for (int i = 0; i < m.nrows (); ++i) {
+        for (int j = 0; j < m.ncols (); ++j)
+            cout << m[i][j] << " ";
+        
+        cout << endl;
+    }
+
 }
