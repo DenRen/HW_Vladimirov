1fe7fc2d4771f92f54acd63d4dab3e3dcb375e64
DenRen <denrenruslan@gmail.com>
2021-12-23 22:06:09 +0300

    Пофиксил баги сборки

--- hidra/build.sh
+++ hidra/build.sh
@@ -2,6 +2,6 @@
 
 mkdir build &> /dev/null
 cd build && cmake -DCMAKE_BUILD_TYPE=Release ..
-cmake --build .
+cmake --build . -j
 
 valgrind -q ./run_tests --gtest_repeat=1 && ./run_tests --gtest_repeat=3
