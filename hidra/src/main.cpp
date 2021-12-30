07b1c219dc3c69ec47c875f23666826ffe6e02ca
DenRen <denrenruslan@gmail.com>
2021-12-30 13:43:05 +0300

    Сделал проверку на ввод

--- hidra/src/main.cpp
+++ hidra/src/main.cpp
@@ -21,26 +21,55 @@ main (int argc, char* argv[]) {
     }
 }
 
+bool
+check_on_pow_2 (std::size_t num) {
+    return (num & (num - 1)) == 0 && num > 0;
+}
+
+template <typename T>
+std::istream&
+operator >> (std::istream& is, std::vector <T>& vec) {
+    std::size_t size = 0;
+    std::cin >> size;
+    
+    if (!check_on_pow_2 (size)) {
+        throw std::invalid_argument ("N is not a power 2");
+    }
+
+    vec.reserve (vec.size () + size);
+
+    for (std::size_t i = 0; i < size; ++i) {
+        T value;
+        std::cin >> value;
+        if (std::cin.fail ()) {
+            throw std::invalid_argument ("Failed to input of numbers.");
+        }
+        vec.emplace_back (std::move (value));
+    }
+
+    return is;
+}
+
 void
 sort_cin () {
     hidra::DeviceProvider device_provider;
     cl::Device device (device_provider.getDefaultDevice ());
     hidra::Sorter sorter (device);
 
-    // --------------------------------------------------------
-
-    std::size_t size = 0;
-    std::cin >> size;
-
     std::vector <int> arr;
-    arr.reserve (size);
-    for (std::size_t i = 0; i < size; ++i) {
-        int value = 0;
-        std::cin >> value;
-        arr.push_back (value);
-    }
+    try {
+        std::cin >> arr;
+    } catch (std::invalid_argument& exc) {
+        const auto example =
+            "\nEnter the N - quantity numbers (N = 2 ^ m) and these numbers.\n"
+            "Example:\n"
+            "4\n"
+            "64 21 32 -78\n"
+            "\nAfter clicking Enter you will receive:\n"
+            "-78 21 32 64\n";
 
-    // --------------------------------------------------------
+        throw std::invalid_argument (std::string (exc.what ()) + example);
+    }
 
     sorter.sort (arr);
     std::cout << arr << std::endl;
